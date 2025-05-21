75d0f04ea44d0fc375e53aef451670ef7a557afe
kangjia <ikangjia.cn@outlook.com>
2025-05-21 17:19:35 +0800

    feat: 为博客系统添加新组件和文章，重构布局结构，移除旧版欢迎页面和配置

---
+++ src/ilvme.config.ts
@@ -0,0 +1,11 @@
+export const SiteConfig = {
+    title: '聪明的墨菲特',
+    description: 'ilvme',
+    author: 'ilvme',
+    siteUrl: 'https://ilvme.com',
+    social: {
+        twitter: 'ilvme',
+    },
+    icon: 'src/images/icon.png',
+    keywords: ['ilvme', 'ilvme.com', 'ilvme.site'],
+}
