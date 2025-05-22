a9f5ea5f1b44e603c85d4af8a51c8c86f97412ae
kangjia <ikangjia.cn@outlook.com>
2025-05-22 13:10:20 +0800

    chore: 更新 Markdown 渲染配置，添加新博客文章及

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  markdown: {
+    shikiConfig: {
+      // 从 Shiki 的内置主题中选择 (或者添加你自己的)
+      // https://shiki.style/themes
+      // theme: 'vesper',
+    },
   }
 });
